54a938b49b7a427b2b42a52d1dcee07abecf2652
Dishant1901 <dishantsinghom@gmail.com>
2023-07-04 18:38:45 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import {Route, Routes} from 'react-router-dom'
+import IndexPage from './pages/indexPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route index element ={<IndexPage/>} />
+    </Routes>
+    
+   
+  );
+}
+
+export default App;
